4a8f6645317638267bf0efd0e698d235812152af
ggomyong <63125569+ggomyong@users.noreply.github.com>
2020-04-10 20:39:28 -0400

    addition of bulkupload

--- app.js
+++ app.js
@@ -11,17 +11,53 @@ app.use(bodyParser.urlencoded({limit: "50mb", extended: true, parameterLimit:500
 app.use(express.json({limit: '50mb'}));
 app.use(express.urlencoded({limit: '50mb'}));
 
-app.get('/api/upload', (req, res) => {
+app.get('/api/download', (req, res) => {
+    //gather all ICRs and combine it and then return it to the requestor
+    let fs = require('fs');
+    let list=JSON.parse(fs.readFileSync('./database/IcrMaster.icr')).members;
+    let contents=[];
+
+    for (let x=0; x<list.length; x++) {
+      let id=list[x];
+      if (Number(id)>0) {
+        let path='./database/'+id+'.icr';
+        if (fs.existsSync(path)) {
+          let icr=JSON.parse(fs.readFileSync(path));
+          contents.push(icr);
+        }
+
+      }
+
+    }
+
     res.json({
-        'message': 'hello'
+        'message': contents
     });
+});
+
+app.post('/api/bulkUpload', (req,res) =>{
+  let fs = require('fs');
+  let content=req.body;
+  //console.log(content);
+  for (let x=0; x<content.length; x++) {
+    let icr=content[x];
+    console.log(JSON.stringify(icr));
+    fs.writeFile('./database/'+icr.id+'.icr', JSON.stringify(icr), function (err) {
+      if (err) throw err;
+      console.log('Saved!');
+    });
+  }
+  res.json({
+      'message': 'File uploaded successfully'
+  });
 });
 
 app.post('/api/upload', (req, res) => {
   let fs = require('fs');
   let content=JSON.stringify(req.body);
+  let id=req.body.id;
 
-  fs.writeFile('./database/icrs.txt', content, function (err) {
+  fs.writeFileSync('./database/'+id+'.icr', content, function (err) {
     if (err) throw err;
     console.log('Saved!');
   });
